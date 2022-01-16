327c6017780b295bd03722c65b369e5657cb0a77
Stefan Buck <dev@stefanbuck.com>
2022-01-17 00:36:39 +0100

    New unistall form

--- pages/goodbye.js
+++ pages/goodbye.js
@@ -1,44 +1,82 @@
 import Container from "../components/container";
+import Bowser from "bowser";
 
 function GoodBye() {
+  let browserName = '';
+  try{
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    browserName = browser.getBrowser().name
+  }catch(error) {
+
+  }
+
   return (
     <>
       <style jsx>{`
-      iframe {
-          margin-top: 1px;
+      textarea {
+        width: 100%;
+        max-width: 400px;
+        height: 8rem;
+        border-radius: 6px;
+        padding: 0.5rem;
       }
-  .loading {
-    display: inline-block;
-    width: 80px;
-    height: 80px;
-    margin-top: 50px;
-  }
-  .loading:after {
-    content: " ";
-    display: block;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border-radius: 50%;
-    border: 6px solid #fd4eff;
-    border-color: #fd4eff transparent #fd4eff transparent;
-    animation: loading 1.2s linear infinite;
-  }
-  @keyframes loading {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-  
+
+      p {
+        width: 100%;
+        max-width: 400px;
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 8rem;
+      }
+
+      button {
+          border: none;
+          margin-top: 1rem;
+          font-size: 14px;
+          font-weight: 700;
+          line-height: 46px;
+          display: inline-block;
+          box-sizing: border-box;
+          height: 46px;
+          padding: 0 22px;
+          cursor: pointer;
+          text-transform: uppercase;
+          color: #fff;
+          border-radius: calc(46px / 2);
+          text-decoration: none;
+          background-color: #1c1f2b;
+        }
+
+        .button:hover {
+          text-decoration: none;
+        }
+
       `}</style>
       <Container center={true}>
-        <div className="loading"></div>
-        <iframe onLoad={function() { 
-            document.querySelector('.loading').remove()
-         }} src="https://docs.google.com/forms/d/e/1FAIpQLSfAmWxqILHEOoQ4x8bN-OOyU08ANy6AphQTBCy1T23sTjtu9Q/viewform?embedded=true" scrolling="no" height="1080" width="100%" frameBorder="0" marginHeight="0" marginWidth="0"></iframe>
+      <h1>Sorry to see you go</h1>
+
+      <form className="flex flex-col gap-3 w-full" method="POST" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeKgspy8YDCFhKevW2wAO1hZYsIMdTruDRkBFNb7syuq82eaA/formResponse">
+          <p>Please help us understand what didn't work so we can improve OctoLinker for you and others. Thank you.</p>
+          <textarea
+            type='text'
+            name='entry.1798064257'
+            required
+            defaultValue="I uninstalled because "
+            />
+          <input
+            type='text'
+            name='entry.325768311'
+            value={browserName}
+            readOnly={true}
+            hidden
+            />
+
+            <button type="submit">Submit response</button>
+          </form>
       </Container>
     </>
   );
