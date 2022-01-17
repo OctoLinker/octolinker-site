83e846e5b79055b551f36b614ed11c607f205331
Stefan Buck <dev@stefanbuck.com>
2022-01-17 22:00:31 +0100

    enhance uninstall form

--- pages/goodbye.js
+++ pages/goodbye.js
@@ -1,33 +1,45 @@
 import Container from "../components/container";
 import Bowser from "bowser";
+import { useRef, useState } from "react";
 
 function GoodBye() {
   let browserName = '';
-  try{
+  try {
     const browser = Bowser.getParser(window.navigator.userAgent);
     browserName = browser.getBrowser().name
-  }catch(error) {
+  } catch (error) {
 
   }
+
+  const [message, setMessage] = useState("I uninstalled it because ")
+  const [hint, showHint] = useState(false)
+  const ref = useRef();
+
+  const reasons = [
+    'Doesn\'t have the features I need',
+    'There were technical issues',
+    'I had performance issues',
+    'I don\'t use it',
+    'I have privacy concerns',
+  ]
+
+  const issueUrl = `https://github.com/OctoLinker/OctoLinker/issues/new?labels=feedback&title=Feedback&body=${encodeURI(message)}`
+
 
   return (
     <>
       <style jsx>{`
       textarea {
         width: 100%;
-        max-width: 400px;
         height: 8rem;
         border-radius: 6px;
         padding: 0.5rem;
       }
 
-      p {
-        width: 100%;
-        max-width: 400px;
-      }
 
-      form {
-        display: flex;
+
+      .container {
+        outline: 1px solid green;
         flex-direction: column;
         align-items: center;
         margin-bottom: 8rem;
@@ -35,7 +47,6 @@ function GoodBye() {
 
       button {
           border: none;
-          margin-top: 1rem;
           font-size: 14px;
           font-weight: 700;
           line-height: 46px;
@@ -43,6 +54,8 @@ function GoodBye() {
           box-sizing: border-box;
           height: 46px;
           padding: 0 22px;
+          margin-top: 1rem;
+          margin-right: 1rem;
           cursor: pointer;
           text-transform: uppercase;
           color: #fff;
@@ -51,32 +64,84 @@ function GoodBye() {
           background-color: #1c1f2b;
         }
 
+        a {
+          color: #1c1f2b;
+        }
+
+        a:hover {
+          text-decoration: underline;
+        }
+
         .button:hover {
           text-decoration: none;
         }
 
+        ul {
+          display: flex;
+          flex-direction: column;
+          gap: 4px;
+          margin: 0;
+          padding: 0 0 0 1rem;
+          list-style-position: inside;
+        }
+
+        li {
+          cursor: pointer;
+        }
+
+        li:hover {
+          opacity: 0.7;
+        }
+
+        h3 {
+          padding-top: 2rem;
+          margin-bottom: 0.5rem;
+        }
+
+        @media only screen and (min-width: 768px) {
+          .inner {
+            padding: 0 150px 3rem 150px;
+          }
+        }
       `}</style>
-      <Container center={true}>
-      <h1>Sorry to see you go</h1>
+      <Container>
+        <h1>Sorry to see you go</h1>
+        <div className="inner">
+          Please help us understand what didn't work so we can improve OctoLinker for you and others. Thank you.
 
-      <form className="flex flex-col gap-3 w-full" method="POST" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeKgspy8YDCFhKevW2wAO1hZYsIMdTruDRkBFNb7syuq82eaA/formResponse">
-          <p>Please help us understand what didn't work so we can improve OctoLinker for you and others. Thank you.</p>
-          <textarea
-            type='text'
-            name='entry.1798064257'
-            required
-            defaultValue="I uninstalled because "
-            />
-          <input
-            type='text'
-            name='entry.325768311'
-            value={browserName}
-            readOnly={true}
-            hidden
-            />
 
-            <button type="submit">Submit response</button>
+          <h3>Choose a reason</h3>
+          <ul>
+            {reasons.map(reason => {
+              return (<li onClick={() => {
+                showHint(true)
+                setMessage(`${reason} `)
+                ref.current.focus();
+              }}>{reason}</li>)
+            })}
+          </ul>
+
+          <h3>or write your feedback</h3>
+          <form method="POST" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeKgspy8YDCFhKevW2wAO1hZYsIMdTruDRkBFNb7syuq82eaA/formResponse">
+            <textarea
+              ref={ref}
+              type='text'
+              name='entry.1798064257'
+              required
+              onChange={(evt) => setMessage(evt.target.value)}
+              value={message}
+            />
+            <input
+              type='text'
+              name='entry.325768311'
+              value={browserName}
+              readOnly={true}
+              hidden
+            />
+            <button type="submit">Submit feedback</button>
+            <a href={issueUrl}>or submit as GitHub issue</a>
           </form>
+        </div>
       </Container>
     </>
   );
